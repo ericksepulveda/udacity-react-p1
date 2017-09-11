e9fd8dc0335d9e48c9c638657b8bdcd5b03a6a9e
ericksepulveda <erick.sepulveda@alumnos.usm.cl>
2017-09-11 08:33:48 -0300

    wip project

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
-    books: require('../books.json'),
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: require('../books.json')
+  }
+
+  componentDidMount() {
+    // BooksAPI.getAll().then((books) => {
+    //   this.setState({ books })
+    // })
   }
 
   render() {
     const { books } = this.state
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div>
+        <Route
+          exact path='/' render={() => (
             <ListBooks books={books}/>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+        )}/>
+        <Route
+          path='/search'
+          component={Search}
+        />
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import Book from './Book'
 
 const shelves = [
@@ -8,6 +10,10 @@ const shelves = [
 ]
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array
+  }
+
   render() {
     const { books } = this.props
 
@@ -18,30 +24,62 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            { shelves.map(s => (
-              <div key={s.value} className="bookshelf">
-                <h2 className="bookshelf-title">{s.display}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books.filter(b => b.status === s.value).map((b) => (
-                      <li key={b.title}>
-                        <Book
-                          title={b.title}
-                          author={b.author}
-                          imageURL={b.imageURL}
-                          status={b.status}
-                        />
-                      </li>
-                    ))}
-                  </ol>
-                </div>
-              </div>
+            { shelves.map(shelve => (
+              <Shelve
+                books={books}
+                shelve={shelve}
+              />
             ))}
           </div>
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
   }
+}
+
+function Shelve(props) {
+  const { shelve, books } = props
+  return (
+    <div key={shelve.value} className="bookshelf">
+      <h2 className="bookshelf-title">{shelve.display}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            books
+              .filter(hasValue(shelve.value))
+              .map(toBookListItem)
+          }
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+
+function hasValue(value) {
+  return (book) => book.status === value
+}
+
+export function toBookListItem(book) {
+  return (
+    <li key={book.title}>
+      {toBook(book)}
+    </li>
+  )
+}
+
+function toBook(book) {
+  return (
+    <Book
+      title={book.title}
+      author={book.author}
+      imageURL={book.imageURL}
+      status={book.status}
+    />
+  )
 }
 
 export default ListBooks
